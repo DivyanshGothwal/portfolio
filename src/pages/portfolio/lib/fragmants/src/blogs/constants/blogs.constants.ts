ef833a849682c151983d86cf091552dfb3826981
Divyansh Gothwal <shotsdivyansh@gmail.com>
2023-05-25 23:55:11 +0530

    changes for new blog

--- src/pages/portfolio/lib/fragmants/src/blogs/constants/blogs.constants.ts
+++ src/pages/portfolio/lib/fragmants/src/blogs/constants/blogs.constants.ts
@@ -7,5 +7,18 @@ export const blogs = [{
   },
   imageUrl: 'https://res.cloudinary.com/practicaldev/image/fetch/s--zfSMRrnT--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/tvx0oi8vybr4b7rmcmk2.jpg',
   heading: 'Creating CI/CD pipeline using triggers of google cloud for web app written in Spring Boot(java) and React.js',
-  content: 'Hello allasdasdasd. This is my first post so bear with me',
+  content: `Hello all😊👋
+  This is my first post so bear with me.
+  
+  In this article, I am going to tell you guys how I was able to automate the testing, building and deployment(on GCP APP ENGINE) of my web app written in React and Spring Boot ( code base on GitHub) using google cloud trigger.`,
+}, {
+  link: 'https://dev.to/divyanshgothwal/settimeout-gotchas-with-react-hooks-1c5j',
+  postedOn: {
+    date: '25',
+    month: 'May',
+    year: '2023',
+  },
+  imageUrl: 'https://res.cloudinary.com/practicaldev/image/fetch/s--smeI3Sr6--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/enosqea7uzg5f0klc5jh.png',
+  heading: 'SetTimeout Gotchas with React Hooks',
+  content: 'In this article we are going to understand Gotchas of using SetTimeout callback function to update state.',
 }];
